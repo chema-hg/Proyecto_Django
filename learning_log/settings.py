d1e81589eb7f3d4f3e2e9c79e99ba2f4182b2439
chema_hg <64953146+chema-hg@users.noreply.github.com>
2021-12-18 19:14:38 +0100

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&_qg+odvn+^v2!#vg32iq4&gl*n+dqij9#05k!8&dxjg&h1vgv'
+SECRET_KEY = 'clave supersecreta'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
