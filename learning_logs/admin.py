effe9afe788d65241fae1c377911ed30069b764a
chema_hg <honrajm@yahoo.es>
2021-12-18 20:29:50 +0100

    El sitio admin de Django, crear un superusuario, registrar un modelo con el sitio admin, añadir temas, definir el modelo Entry, migrarlo y añadirlo al sitio admin.

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+# Este código importa primero el módelo que queremos registrar Topic.
+# El punto delante de models le dice a Django que busque models.py en el mismo directorio
+# que admin.py
+from .models import Topic
+admin.site.register(Topic)
+# le dice a Django que gestione nuestro modelo a través del sitio admin.
+
+from .models import Entry
+admin.site.register(Entry)
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -25,3 +25,27 @@ class Topic(models.Model):
 # Para ver los distintos tipos de campos que puedemos usar en un modelo, vease:
 # https://docs.djangoproject.com/en/4.0/ref/models/fields/
 
+class Entry(models.Model):
+    """Para poder registrar algo especifico aprendido sobre un tema"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    data_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Devuelve una representación del modelo como cadena."""
+        return f"{self.text[:50]}..."
+
+# Para que un usuario registre lo que ha aprendido sobre los temas que hemos definido en Topic, cada
+# entrada debe asociarse a un tema en particular. Esta relación se conoce como de un o a muchos, lo que
+# significa que se pueden asociar varias entradas con un mismo tema.
+# La clase Entry hereda de la clase base Model de Django, el primer atributo topic es una instancia de
+# ForeignKey. Una clave foranea es un termino relacionado con las bases de datos. Es una referencia a
+# otro registro de la base. Este es el código que conecta una entrada con cada tema especifico. A cada
+# entrada se le asigna una clave o ID cuando se crea. Cuando Django necesita establecer una conexión entre
+# los datos, utiliza la clave asociada con cada uno. El argumento on_delete=models.CASCADE dice a Django
+# que cuando se borre un tema se borren tambien todas las entradas asociadas al mismo.
+# como hemos añadido un modelo tenemos que crear la migración y aplicarla y registrar Entry con el sitio
+# admin.
